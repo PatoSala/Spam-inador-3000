c7f767ee6997666569140762ee523a46a69a32d3
PatoSala <patosala998@gmail.com>
2021-06-22 21:16:07 -0300

    added features

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const qrcode = require('qrcode-terminal');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
+const { Client } = require('whatsapp-web.js');
 /* const name = require("./name"); */
 
-const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
 let phone;
 let iterations;
 let body;
+let disturbanceFlag = false;
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+function doNotDisturb() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "options",
+            message: "Do not disturb ON/OFF",
+            choices: [
+                "On",
+                "Off"
+            ]
+        }
+    ]).then(answer => {
+        if (answer.options == "On") {
 
-client.on('ready', () => {
-    console.log("");
-    console.log(chalk.bgWhite.bold.black("SPAMAITOR-3000"));
-    console.log("");
-    start();
-});
+            console.log("");
+            console.log("Do not disturb is " + chalk.bold.red("ON"));
+            console.log("");
 
-client.initialize();
+            disturbanceFlag = true;
 
-function start() {
+        } else if (answer.options == "Off") {
+
+            console.log("");
+            console.log("Do not disturb is " + chalk.bold.red("OFF"));
+            console.log("");
+
+            disturbanceFlag = false;
+
+        }
+
+        menu();
+
+    })
+}
+
+function disturbSomeone() {
     inquirer.prompt([
         {
             type: "number",
@@ -50,22 +73,59 @@ function start() {
                 }
             ]).then(answer => {
                 body = answer.body;
-                console.log("Here we go...");
+                console.log(chalk.bold("Here we go..."));
 
                 for (let i = 0; i <= iterations; i++) {
                     client.sendMessage(phone, body);
                 }
 
-                console.log("It's done.")
-                start();
+                console.log("");
+                console.log(chalk.bold("It's done."));
+                menu();
             })
 
         })
     })
 }
 
+function menu() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "options",
+            message: "What do you want to do?",
+            choices: [
+                "Do not disturb",
+                "Disturb someone"
+            ]
+        }
+    ]).then(answer => {
+        if (answer.options == "Do not disturb") {
+            doNotDisturb();
+        } else if (answer.options == "Disturb someone") {
+            disturbSomeone();
+        }
+    })
+}
 
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
 
+client.on('ready', () => {
+    console.log("");
+    console.log(chalk.bgWhite.bold.black("SPAM-INADOR-3000"));
+    console.log("");
+    menu();
+});
 
+client.initialize();
 
+if (disturbanceFlag == true) {
+    client.on("message", function(message) {
+        for (let i = 0; i <= 10; i++) {
+            client.sendMessage(message.from, "Do not disturb meee");
+        }
+    })
+}
 
